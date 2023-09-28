628c9e82b6135a5741cc22dcf9f3afadb3dd40cd
Jelle Verheyen <jelle.verheyen@pm.me>
2023-09-28 11:49:02 +0200

    feat(config): Add class for retrieving cross-platform application directories

---
+++ src/PowerFile.CommandLine/Config/CrossPlatformDirectories.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace PowerFile.CommandLine.Config;
+
+public abstract class CrossPlatformDirectories(string applicationName)
+{
+    public abstract string Data { get; }
+    public abstract string Config { get; }
+    public abstract string Cache { get; }
+    public abstract string Log { get; }
+    public abstract string Temp { get; }
+}
+
+public class LinuxPowerFileDirectories(string applicationName) : CrossPlatformDirectories(applicationName)
+{
+    private readonly IDictionary<string, string> _map = new Dictionary<string, string>()
+    {
+        { nameof(Data), Path.Combine(Environment.GetEnvironmentVariable("XDG_DATA_HOME") ?? "~/.local/share", applicationName)},
+        { nameof(Config), Path.Combine(Environment.GetEnvironmentVariable("XDG_CONFIG_HOME") ?? "~/.config", applicationName )},
+        { nameof(Cache), Path.Combine(Environment.GetEnvironmentVariable("XDC_CACHE_HOME") ?? "~/.cache", applicationName )},
+        { nameof(Log), Path.Combine(Environment.GetEnvironmentVariable("XDC_STATE_HOME") ?? "~/.local/state", applicationName )},
+        { nameof(Temp), Path.Combine(Path.GetTempPath(), applicationName) },
+    };
+
+    public override string Data => _map[nameof(Data)];
+    public override string Config => _map[nameof(Config)];
+    public override string Cache => _map[nameof(Cache)];
+    public override string Log => _map[nameof(Log)];
+    public override string Temp => _map[nameof(Temp)];
+}
+
+public class MacPowerFileDirectories(string applicationName) : CrossPlatformDirectories(applicationName)
+{
+    private readonly IDictionary<string, string> _map = new Dictionary<string, string>()
+    {
+        { nameof(Data), Path.Combine("~/Library/Application Support", applicationName )},
+        { nameof(Config), Path.Combine("~/Library/Preferences", applicationName )},
+        { nameof(Cache), Path.Combine("~/Library/Caches", applicationName )},
+        { nameof(Log), Path.Combine("~/Library/Logs", applicationName )},
+        { nameof(Temp), Path.Combine(Path.GetTempPath(), applicationName) }
+    };
+
+    public override string Data => _map[nameof(Data)];
+    public override string Config => _map[nameof(Config)];
+    public override string Cache => _map[nameof(Cache)];
+    public override string Log => _map[nameof(Log)];
+    public override string Temp => _map[nameof(Temp)];
+}
+
+public class WindowsPowerFileDirectories(string applicationName) : CrossPlatformDirectories(applicationName)
+{
+    private readonly IDictionary<string, string> _map = new Dictionary<string, string>()
+    {
+        { nameof(Data), Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Data")},
+        { nameof(Config), Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), applicationName, "Config")},
+        { nameof(Cache), Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), applicationName, "Cache" )},
+        { nameof(Log), Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), applicationName, "Log")},
+        { nameof(Temp), Path.Combine(Path.GetTempPath(), applicationName) }
+    };
+
+    public override string Data => _map[nameof(Data)];
+    public override string Config => _map[nameof(Config)];
+    public override string Cache => _map[nameof(Cache)];
+    public override string Log => _map[nameof(Log)];
+    public override string Temp => _map[nameof(Temp)];
+}
--- src/PowerFile.CommandLine/DependencyInjection.cs
+++ src/PowerFile.CommandLine/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using CommandLine;
 using Microsoft.Extensions.DependencyInjection;
+using PowerFile.CommandLine.Config;
 using PowerFile.CommandLine.Verbs;
 using PowerFile.Core;
 using PowerFile.Core.Templating;
@@ -11,8 +12,28 @@ namespace PowerFile.CommandLine;
 
 public static class DependencyInjection
 {
+    public static IServiceCollection AddCrossPlatformPaths(this IServiceCollection services, string applicationName)
+    {
+        services.AddSingleton<CrossPlatformDirectories>(x =>
+        {
+            CrossPlatformDirectories? paths = null;
+
+            if (OperatingSystem.IsWindows())
+                paths = new WindowsPowerFileDirectories(applicationName);
+            if (OperatingSystem.IsLinux())
+                paths = new LinuxPowerFileDirectories(applicationName);
+            if (OperatingSystem.IsMacOS())
+                paths = new MacPowerFileDirectories(applicationName);
+
+            return paths ?? throw new InvalidOperationException($"Unsupported environment, only Windows, Linux, and MacOS are supported.");
+        });
+
+        return services;
+    }
+    
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddCrossPlatformPaths("PowerFile");
         services.AddTransient<IPowerFileCommandLineApplication, DefaultPowerFileCommandLineApplication>();
         services.AddTransient<IVerbHandlerFactory, DefaultVerbHandlerFactory>();
         services.AddLogging();
@@ -35,8 +56,22 @@ public static class DependencyInjection
     {
         services.AddOptions<TemplatingOptions>().Configure((opts) =>
         {
-            opts.TemplatesBasePath = @"C:\Users\Jelle\PowerFile\templates";
-            opts.IndexPath = @"C:\Users\Jelle\PowerFile\templates/config/templates.index.json";
+            var provider = services.BuildServiceProvider();
+            var dirs = provider.GetRequiredService<CrossPlatformDirectories>();
+            
+            opts.TemplatesBasePath = Path.Combine(dirs.Config, "templates");
+            opts.IndexPath = Path.Combine(dirs.Cache, "templates.index");
+
+            if (!Directory.Exists(opts.TemplatesBasePath))
+            {
+                Directory.CreateDirectory(opts.TemplatesBasePath);
+            }
+
+            var indexDirectory = Path.GetDirectoryName(opts.IndexPath);
+            if (!Directory.Exists(indexDirectory))
+            {
+                Directory.CreateDirectory(indexDirectory!);
+            }
         });
         
         services.AddSingleton<IPowerFileTemplateManager, TemplateManager>();
